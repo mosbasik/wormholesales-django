3aa6ddf98038aeac082a9e49d478f2e09f4a7e52
Peter Henry <phenry@caltech.edu>
2015-08-06 03:41:18 -0600

    did some styling on the order listing page

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# serving static files
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('main.urls', namespace='main')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
