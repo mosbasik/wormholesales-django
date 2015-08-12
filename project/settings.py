e82e6e1bf759dea57be14b8a284b3fa9fb05fc01
Peter Henry <phenry@caltech.edu>
2015-08-12 12:36:43 -0600

    presentation changes to static boxes

--- project/settings.py
+++ project/settings.py
@@ -33,8 +33,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
     'django.contrib.humanize',
+    'mathfilters',
+    'main',
 )
 
 MIDDLEWARE_CLASSES = (
