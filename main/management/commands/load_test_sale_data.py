5a7434090cc19185d3cbeca64c26ab2378aead90
Peter Henry <phenry@caltech.edu>
2015-08-18 00:51:32 -0600

    updated sell data import script to check names

--- main/management/commands/load_test_sale_data.py
+++ main/management/commands/load_test_sale_data.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 from main.models import Character, System, Order
 
 import json
-import re
+from lxml import etree
 import requests
 
 
@@ -13,15 +13,37 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         with open('main/static/main/test_sale_data.json', 'r') as f:
             data = json.load(f)
-            self._populateCharacters(data)
-            self._populateOrders(data)
+            self._wipe_data()
+            self._populate_characters(data)
+            self._populate_orders(data)
 
-    def _populateCharacters(self, data):
+    def _wipe_data(self):
+        Order.objects.all().delete()
+        Character.objects.all().delete()
+
+    def _populate_characters(self, data):
         for order in data['order']:
-            char, created = Character.objects.get_or_create(
-                    name=order['contact_name'])
+            name = order['contact_name']
 
-    def _populateOrders(self, data):
+            # if name is already in our database, skip it and continue
+            if Character.objects.filter(name=name).exists():
+                continue
+
+            # if name is not already cached in our database, check the API
+            else:
+                url_stub = 'https://api.eveonline.com/eve/CharacterID.xml.aspx?names='
+                response = requests.get(url_stub + name)
+                tree = etree.XML(response.content)
+                character_id_xpath = '/eveapi/result/rowset/row/@characterID'
+                character_id = int(tree.xpath(character_id_xpath)[0])
+
+                # make sure no garbage data is being introduced
+                assert character_id != 0
+
+                # create character
+                Character.objects.create(id=character_id, name=name)
+
+    def _populate_orders(self, data):
         for order in data['order']:
             order = Order.objects.create(
                     user=User.objects.get(
@@ -31,4 +53,5 @@ class Command(BaseCommand):
                     system=System.objects.get(
                             j_code=order['system']),
                     price=order['price'],
-                    information=order['information'])
+                    information=order['information'],
+                    is_sell=True)
